99be39b51694d9bea7a1ba174ecbeff07cd90935
F2x <f2coltd@F2s-iMac.local>
2025-01-04 20:08:17 +0700

    Dashbard card block

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,56 @@
 
 // import { auth, signOut } from '../utils/auth'
 // import { redirect } from 'next/navigation';
+import DashboardCardBlock from '@/components/dashbaord/DashboardCardBlock';
 import { requireAuth } from '../utils/hooks';
+import { link } from 'fs';
+import { Activity, Coins, DollarSign, User, Waves } from 'lucide-react';
 
 const DashboardPage = async () => {
   const session = await requireAuth();
 
-    return (
-      <div>
-        
-        <h1>Dashboad Page</h1>
-        {/* <form
-          action={ async () => {
-            "use server"
-            await signOut()
-          }}
-        >
-          <button type="submit">Sign Out</button>
-      </form> */}
+  const CardBlocks = [
+    {
+      id: 1,
+      title: 'Toal Revenue',
+      description: 'Last 30 days revenue',
+      total: 79800.00,
+      link: '/revenue',
+      icon: <DollarSign className='size-4 text-muted-foreground' />
+    },
+    {
+      id: 2,
+      title: 'Total Invoices',
+      description: 'Total invoices created',
+      total: 38,
+      link: '/revenue',
+      icon: <User className='size-4 text-muted-foreground' />
+    },
+    {
+      id: 3,
+      title: 'Paid Invoices',
+      description: 'Total paid invoices',
+      total: 38,
+      link: '/revenue',
+      icon: <Coins className='size-4 text-muted-foreground' />
+    },
+    {
+      id: 4,
+      title: 'Pending Invoices',
+      description: 'Pending invoices waiting for payment',
+      total: 38,
+      link: '/revenue',
+      icon: <Activity className='size-4 text-muted-foreground' />
+    },
+    
+  ];
 
-      </div>
+    return (
+      <>
+        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-8'>
+          {CardBlocks.map((card) => ((<DashboardCardBlock key={card.id} title={card.title} description={card.description} total={card.total} link={card.link} icon={card.icon}  />)))}
+        </div>
+      </>
     )
   }
 
---
+++ components/dashbaord/DashboardCardBlock.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
+import { Plus, PlusCircle } from 'lucide-react';
+
+interface DashboardCardProps {
+  title: string;
+  description: string;
+  total: number;
+  link: string,
+  icon: any;
+}
+
+const DashboardCardBlock = ({title, description, total, link, icon}: DashboardCardProps) => {
+  return (
+    
+      <Card>
+        <CardHeader className='flex flex-row items-center justify-between'>
+          <CardTitle className='text-sm font-medium' >
+            {title}
+          </CardTitle>
+            {icon}
+          {/* <CardDescription>
+            {description}
+          </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <h2 className='flex items-center text-2xl font-bold'><Plus/>{total}</h2>
+          <p className='text-xs text-muted-foreground'>{description}</p>
+        </CardContent>
+      </Card>
+    
+  )
+}
+
+export default DashboardCardBlock
