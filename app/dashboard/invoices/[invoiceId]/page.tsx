985d46cced374cd22045fe964307f8ad609a805b
F2x <f2coltd@F2s-iMac.local>
2025-01-28 11:04:08 +0700

    update params to use Promise<{invoiceId: string}>

--- app/dashboard/invoices/[invoiceId]/page.tsx
+++ app/dashboard/invoices/[invoiceId]/page.tsx
@@ -5,14 +5,14 @@ import { Prisma } from '@prisma/client'
 import React from 'react'
 
 
-const EditInvoiceRoute = async ({ params }: { params: { invoiceId: string }} ) => {
+const EditInvoiceRoute = async ({ params }: { params: Promise<{invoiceId: string}> }) => {
   const session = await requireAuth()
   const userId = session.user?.id;
   if (!userId) {
     throw new Error('User ID is undefined');
   }
 
-  const { invoiceId } = params;
+  const { invoiceId } = await params;
 
   interface InvoiceProps {
     data: Prisma.InvoiceGetPayload<{}>;
