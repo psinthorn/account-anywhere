5b64b201c971f198acdfbc225fe011416b245f80
F2x <f2coltd@F2s-iMac.local>
2025-01-29 05:52:00 +0700

    implement signout route

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
       <Navbar />
       <Hero />
-      <SignOut/>
+      {/* <SignOut/> */}
     </main>
   );
 }
